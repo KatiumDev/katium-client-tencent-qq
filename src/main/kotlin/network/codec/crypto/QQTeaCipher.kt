5559c3701e8b4267b42bad90053a26c9ca33bb82
xtexChooser <zbwxtex@qq.com>
2022-05-03 08:16:33 +0800

    feat: TEA cipher and QQTEA cipher

---
+++ src/main/kotlin/network/codec/crypto/QQTeaCipher.kt
@@ -0,0 +1,97 @@
+/*
+ * Katium Client Tencent QQ: Tencent QQ protocol implementation for Katium
+ * Copyright (C) 2022  Katium Project
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package katium.client.qq.network.codec.crypto
+
+import io.netty.buffer.ByteBuf
+import katium.core.util.getULong
+import katium.core.util.setULong
+import kotlin.experimental.and
+import kotlin.random.Random
+
+/**
+ * https://github.com/lz1998/rs-qq/blob/master/rq-engine/src/crypto/qqtea.rs
+ * https://github.com/Mrs4s/MiraiGo/blob/master/binary/tea.go
+ * https://github.com/mamoe/mirai/blob/master/mirai-core/src/commonMain/kotlin/utils/crypto/TEA.kt
+ * https://github.com/takayama-lily/oicq/blob/main/lib/core/tea.ts
+ */
+class QQTeaCipher(val cipher: TeaCipher, val random: Random = Random.Default) {
+
+    fun encrypt(data: ByteBuf): ByteBuf {
+        val fillSize = 9 - ((data.readableBytes() + 1) % 8)
+        val buffer = data.alloc().buffer(1 + fillSize + data.readableBytes() + 7)
+        buffer.writeByte((fillSize - 2) or (random.nextInt() and 0b11111000))
+        for (i in 0 until fillSize) {
+            buffer.writeByte(random.nextInt())
+        }
+        buffer.writeBytes(data)
+            .writeBytes(byteArrayOf(0, 0, 0, 0, 0, 0, 0)) // fill padding
+        var iv1 = 0uL
+        var iv2 = 0uL
+        var holder: ULong
+
+        for (index in 0 until buffer.readableBytes() step 8) {
+            val block = buffer.getULong(index)
+            holder = block xor iv1
+            iv1 = cipher.encrypt(holder)
+            iv1 = iv1 xor iv2
+            iv2 = holder
+            buffer.setULong(index, iv1)
+        }
+
+        return buffer
+    }
+
+    fun decrypt(buffer: ByteBuf): ByteBuf {
+        if (buffer.readableBytes() < 16 || buffer.readableBytes() % 8 != 0) {
+            throw IllegalArgumentException("Size of QQTea encrypted data should greater than 16 and multiplier of 8")
+        }
+        var iv1: ULong
+        var iv2 = 0uL
+        var holder = 0uL
+
+        for (index in buffer.readerIndex() until buffer.writerIndex() step 8) {
+            val block = buffer.getULong(index)
+            iv1 = block
+            iv2 = iv2 xor iv1
+            iv2 = cipher.decrypt(iv2)
+            buffer.setULong(index, iv2 xor holder)
+            holder = iv1
+        }
+
+        val fillSize = (buffer.readByte() and 0b00000111) + 2
+        val dataLength = buffer.readableBytes() - 7 - fillSize
+        return buffer.copy(buffer.readerIndex() + fillSize, dataLength)
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is QQTeaCipher) return false
+        if (cipher != other.cipher) return false
+        if (random != other.random) return false
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = cipher.hashCode()
+        result = 31 * result + random.hashCode()
+        return result
+    }
+
+    override fun toString() = "QQTeaCipher(cipher=$cipher, random=$random)"
+
+}
---
+++ src/main/kotlin/network/codec/crypto/TeaCipher.kt
@@ -0,0 +1,96 @@
+/*
+ * Katium Client Tencent QQ: Tencent QQ protocol implementation for Katium
+ * Copyright (C) 2022  Katium Project
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package katium.client.qq.network.codec.crypto
+
+/**
+ * https://github.com/lz1998/rs-qq/blob/master/rq-engine/src/crypto/qqtea.rs
+ * https://github.com/Mrs4s/MiraiGo/blob/master/binary/tea.go
+ * https://github.com/mamoe/mirai/blob/master/mirai-core/src/commonMain/kotlin/utils/crypto/TEA.kt
+ * https://github.com/takayama-lily/oicq/blob/main/lib/core/tea.ts
+ */
+@Suppress("OPT_IN_IS_NOT_ENABLED")
+@OptIn(ExperimentalUnsignedTypes::class)
+class TeaCipher(val key: UIntArray) {
+
+    companion object {
+
+        const val DELTA = 0x9E3779B9u // (sqrt(5) - 1) * 2 ^ 31
+
+        fun decodeByteKey(bytes: UByteArray): UIntArray {
+            if (bytes.size % 4 != 0) {
+                throw IllegalArgumentException("The size of TEA bytes key should be multiplier of 4")
+            }
+            val result = UIntArray(bytes.size shr 2)
+            for (index in result.indices) {
+                val baseIndex = index * 4
+                result[index] = (bytes[baseIndex].toUInt() shl 24) +
+                        (bytes[baseIndex + 1].toUInt() shl 16) +
+                        (bytes[baseIndex + 2].toUInt() shl 8) +
+                        (bytes[baseIndex + 3].toUInt())
+            }
+            return result
+        }
+
+    }
+
+    init {
+        if (key.size != 4) {
+            throw IllegalArgumentException("The key of TEA crypto should be u32[4]")
+        }
+    }
+
+    fun encrypt(data: ULong): ULong {
+        var sum = 0u
+        var x = (data shr 32).toUInt()
+        var y = data.toUInt()
+
+        for (i in 0u until 16u) {
+            sum += DELTA
+            x += (key[0] + (y shl 4)) xor (y + sum) xor (key[1] + (y shr 5))
+            y += (key[2] + (x shl 4)) xor (x + sum) xor (key[3] + (x shr 5))
+        }
+
+        return (x.toULong() shl 32) or y.toULong()
+    }
+
+    fun decrypt(data: ULong): ULong {
+        var sum = DELTA shl 4
+        var x = (data shr 32).toUInt()
+        var y = data.toUInt()
+
+        for (i in 0u until 16u) {
+            y -= (key[2] + (x shl 4)) xor (x + sum) xor (key[3] + (x shr 5))
+            x -= (key[0] + (y shl 4)) xor (y + sum) xor (key[1] + (y shr 5))
+            sum -= DELTA
+        }
+
+        return (x.toULong() shl 32) or y.toULong()
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is TeaCipher) return false
+        if (key != other.key) return false
+        return true
+    }
+
+    override fun hashCode() = key.hashCode()
+
+    override fun toString() = "TeaCipher(key=$key)"
+
+}
