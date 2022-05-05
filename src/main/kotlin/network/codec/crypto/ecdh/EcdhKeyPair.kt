ef55681569f582e1cc01af55032064fde29d3262
xtexChooser <zbwxtex@qq.com>
2022-05-06 07:54:13 +0800

    feat: ECDH encryption

---
+++ src/main/kotlin/network/codec/crypto/ecdh/EcdhKeyPair.kt
@@ -0,0 +1,76 @@
+/*
+ * Katium Client Tencent QQ: Tencent QQ protocol implementation for Katium
+ * Copyright (C) 2022  Katium Project
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package katium.client.qq.network.codec.crypto.ecdh
+
+import com.google.common.hash.Hashing
+import katium.client.qq.network.codec.crypto.tea.QQTeaCipher
+import org.bouncycastle.jce.provider.BouncyCastleProvider
+import java.security.KeyFactory
+import java.security.KeyPairGenerator
+import java.security.PublicKey
+import java.security.Security
+import java.security.spec.ECGenParameterSpec
+import java.security.spec.X509EncodedKeySpec
+import java.util.*
+import javax.crypto.KeyAgreement
+
+
+data class EcdhKeyPair(
+    val keyVersion: Int,
+    val publicKey: PublicKey,
+    val shareKey: UByteArray
+) {
+
+    val publicKeyEncoded: ByteArray = publicKey.encoded
+    val shareKeyTeaCipher = QQTeaCipher(shareKey)
+
+    companion object {
+
+        init {
+            Security.addProvider(BouncyCastleProvider())
+        }
+
+        @JvmField
+        val X509_PREFIX: ByteArray = HexFormat.of().parseHex("3059301306072a8648ce3d020106082a8648ce3d030107034200")
+
+        val Builtin = create(
+            1,
+            "04EBCA94D733E399B2DB96EACDD3F69A8BB0F74224E2B44E3357812211D2E62EFBC91BB553098E25E33A799ADC7F76FEB208DA7C6522CDB0719A305180CC54A82E"
+        )
+
+        fun create(keyVersion: Int, publicKey: String): EcdhKeyPair {
+            val key =
+                KeyFactory.getInstance("EC")
+                    .generatePublic(X509EncodedKeySpec(X509_PREFIX + HexFormat.of().parseHex(publicKey)))
+            val keyPair = KeyPairGenerator.getInstance("ECDH")
+                .run {
+                    initialize(ECGenParameterSpec("prime256v1"))
+                    generateKeyPair()
+                }
+            val shareKey = KeyAgreement.getInstance("ECDH").run {
+                init(keyPair.private)
+                doPhase(key, true)
+                @Suppress("DEPRECATION")
+                Hashing.md5().hashBytes(generateSecret().copyOf(16)).asBytes().toUByteArray()
+            }
+            return EcdhKeyPair(keyVersion, keyPair.public, shareKey)
+        }
+
+    }
+
+}
