042dd189c8e359f2a753192d17613e4d0c391bde
xtexChooser <zbwxtex@qq.com>
2022-05-28 21:36:53 +0800

    feat: downloading image

--- src/main/kotlin/message/content/QQImage.kt
+++ src/main/kotlin/message/content/QQImage.kt
@@ -2,12 +2,30 @@ package katium.client.qq.message.content
 
 import com.google.protobuf.ByteString
 import katium.core.message.content.Image
+import katium.core.util.okhttp.GlobalHttpClient
+import katium.core.util.okhttp.await
+import katium.core.util.okhttp.expected
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.runBlocking
+import okhttp3.Request
 
-class QQImage(val resourceKey: String, originUrl: String, val md5: ByteString): Image() {
+class QQImage(val resourceKey: String, originUrl: String, val md5: ByteString) : Image() {
 
-    override val contentBytes: ByteArray?
-        get() = TODO("Not yet implemented")
+    override val contentBytes: ByteArray by lazy {
+        runBlocking(CoroutineName("Download QQ Chat Image")) {
+            GlobalHttpClient.newCall(
+                Request.Builder()
+                    .url(contentUrl)
+                    .get()
+                    .build()
+            )
+                .await()
+                .expected(200)
+                .body
+                .bytes()
+        }
+    }
 
-    override val contentUrl: String = "https://c2cpicdw.qpic.cn/$originUrl"
+    override val contentUrl = "https://c2cpicdw.qpic.cn/$originUrl"
 
 }
